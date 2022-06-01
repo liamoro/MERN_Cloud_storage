e89871c284ec4a1f82b7589e4a64ef6ba2396a43
Lia Moro <moro.lia@yandex.ru>
2022-06-01 18:50:24 +0300

    start

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      haksdks
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const authRouter = require('./routes/auth.routes')
+
+const app = express()
+const PORT = config.get('serverPort') || 5000
+
+app.use(express.json())
+app.use('/api/auth', authRouter)
+
+app.use('/', (req, res) => {
+  res.json({message: 'Server started'})
+})
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('dbUrl'))
+
+    app.listen(PORT, () => {
+      console.log('Server was started at port ', PORT)
+    })
+  } catch (err) {
+    
+  }
+}
+
+start()
