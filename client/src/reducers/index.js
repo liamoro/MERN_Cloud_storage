142aa78d9f9f6d2a63752e2e49643974bf9e4e11
Lia Moro <moro.lia@yandex.ru>
2022-06-02 17:33:58 +0300

    remove thunk as externall package

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,5 @@
 import {combineReducers} from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
-import thunk from 'redux-thunk'
 import userReducer from './userReducer'
 import fileReducer from './fileReducer'
 
@@ -11,7 +10,5 @@ const rootReducer = combineReducers({
 })
 
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: [thunk] // передается в applyMiddleware()
-  //, devTools: true // it's default, needs for browser extention 
+  reducer: rootReducer
 })
